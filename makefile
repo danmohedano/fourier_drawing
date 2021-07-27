3b16044966dbd1c57c0a2e62ec5f2773052a120e
Daniel Mohedano Rodriguez <daniel.mohedano@estudiante.uam.es>
2021-07-27 12:11:20 +0200

    Initial project structure

---
+++ makefile
@@ -0,0 +1,50 @@
+INC=./includes
+SRC=./src
+OBJ=./obj
+EXE=fourier.exe
+
+CC = g++
+CFLAGS = -Wall -g
+LIBS =
+SDL_CFLAGS = $(shell sdl2-config --cflags)
+SDL_LDFLAGS = $(shell sdl2-config --libs)
+
+.PHONY: clean_obj clean_exe clean
+
+all: clean setup $(EXE)
+
+setup:
+	DISPLAY=:0
+	export DISPLAY
+	mkdir -p obj 
+
+#~~~~~~~~~~~~~ exe ~~~~~~~~~~~~~#
+
+$(EXE): $(OBJ)/main.o
+	@echo "Building executable..."
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS) $(SDL_CFLAGS) $(SDL_LDFLAGS)
+
+#~~~~~~~~~~~~~ obj ~~~~~~~~~~~~~#
+$(OBJ)/main.o: $(SRC)/main.cpp
+	@echo "Compiling main module..."
+	$(CC) $(CFLAGS) -c $< -o $@ $(SDL_CFLAGS) $(SDL_LDFLAGS)
+
+#~~~~~~~~~~~~~ cleanup ~~~~~~~~~~~~~#
+clean: clean_obj clean_exe
+
+clean_obj:
+	@echo "Cleaning $(OBJ)/..."
+	rm -f $(OBJ)/*.o
+
+clean_exe:
+	@echo "Cleaning exe..."
+	rm -f $(EXE)
+
+#~~~~~~~~~~~~~ test ~~~~~~~~~~~~~#
+test:
+	@echo "Testing $(EXE)..."
+	./$(EXE)
+
+test_valgrind:
+	@echo "Testing with valgrind $(EXE)..."
+	valgrind --leak-check=full ./$(EXE)
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+/**
+ * 27/07/2021
+ * Module: main
+ * -----------------------------------------------------------------------------
+ * Authors:
+ * - Daniel Mohedano <https://github.com/danmohedano>
+ * -----------------------------------------------------------------------------
+ * Main module to execute the program.
+ */
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "../includes/config.h"
+
+int main(void)
+{
+    SDL_Window* gWindow = NULL;
+    SDL_Surface* gScreenSurface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize. Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    else
+    {
+        //Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			return 1;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+    }
+
+    std::cout << "Init correct\n";
+
+    SDL_UpdateWindowSurface(gWindow);
+    SDL_Delay(2000);
+
+
+    //Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+    return 0;
+}
